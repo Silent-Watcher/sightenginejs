307ac6936910cb60bd91a36c1a9518b5323b70e2
Silent-Watcher <alitabatabaee20@gmail.com>
2025-08-15 18:26:41 +0330

    chore(app): initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+	entry: ['src/*'],
+	format: ['esm'], // Keep ESM format
+	outDir: 'dist',
+	dts: true,
+	shims: true,
+	clean: true,
+	minify: true,
+	outExtension: () => ({ js: '.js' }), // Force .js instead of .mjs
+});
